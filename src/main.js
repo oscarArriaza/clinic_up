41ed58d1575d2782f3ec8d89c6b635afe3a18a7a
oscarArriaza <arriaza22598@vk.com>
2024-09-15 12:50:08 +0200

    oscar

--- src/main.js
+++
@@ -1,8 +0,0 @@
-//import './assets/main.css'
-
-import { createApp } from 'vue'
-//import App from './App.vue'
-import Clinic_up from './components/Clinic_up.vue'
-
-//createApp(App).mount('#app')
-createApp(Clinic_up).mount('#clinicUp')
